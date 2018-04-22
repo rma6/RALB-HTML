df3641b6fc66ac6472e588aa175059feeae98d05
Rafael Marinho de Araújo <rma6@cin.ufpe.br>
2018-04-22 17:40:23 -0300

    Delete auth.php

--- auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cpf = $_GET["cpf"]
-$senha = $_GET["senha"]
-$page = file_get_contents('http://www.biblioteca.ufpe.br/pergamum/biblioteca_s/php/login_usu.php?login='.$cpf.'&password='.$senha);
-if (strpos($page, 'div') !== false) echo '0';
-else echo '1';
-?>
